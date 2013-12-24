19b02b6bbc347b739bc25067a196b43e49887372
minde <mindeunix@gmail.com>
2013-12-24 23:29:39 +0200

    Nusprendziau pasalinti extern vicious, sukurti savo timerius tam, kad butu lengviau kontroliuoti sys widgets.

--- widgets/sys/cpu.lua
+++ widgets/sys/cpu.lua
@@ -17,10 +17,65 @@ local line_graph = require("extern.graph.line_graph")
 
 local module = {}
 
+-- Get CPU usage
+local cpu = {}
+local cpu_usage  = {}
+local cpu_total  = {}
+local cpu_active = {}
+function module.cpuUsage()
+    local cpu_lines = {}
+    -- Get CPU stats
+    local f = io.open("/proc/stat")
+    for line in f:lines() do
+        if string.sub(line, 1, 3) ~= "cpu" then break end
+        cpu_lines[#cpu_lines+1] = {}
+        for i in string.gmatch(line, "[%s]+([^%s]+)") do
+            table.insert(cpu_lines[#cpu_lines], i)
+        end
+    end
+    f:close()
+
+    -- Ensure tables are initialized correctly
+    for i = #cpu_total + 1, #cpu_lines do
+        cpu_total[i]  = 0
+        cpu_usage[i]  = 0
+        cpu_active[i] = 0
+    end
+
+    for i, v in ipairs(cpu_lines) do
+        -- Calculate totals
+        local total_new = 0
+        for j = 1, #v do
+            total_new = total_new + v[j]
+        end
+        local active_new = total_new - (v[4] + v[5])
+
+        -- Calculate percentage
+        local diff_total  = total_new - cpu_total[i]
+        local diff_active = active_new - cpu_active[i]
+
+        if diff_total == 0 then diff_total = 1E-6 end
+        cpu_usage[i]      = math.floor((diff_active / diff_total) * 100)
+
+        -- Store totals
+        cpu_total[i]   = total_new
+        cpu_active[i]  = active_new
+    end
+    dbg.dump(cpu_usage)
+    return cpu_usage
+end
+
+-- Timer
+function module.timer()
+    local wtimer = timer{ timeout = 5 }
+    wtimer:connect_signal("timeout", function() end)
+    wtimer:start()
+end
+
+--
 -- Graph
-local graph
 local function widget_graph()
-    graph = line_graph()
+    local graph = line_graph()
     graph:set_height(61)
     graph:set_width(120)
     graph:set_graph_line_color("#0036B8")
@@ -34,11 +89,11 @@ local function widget_graph()
         wibox.layout.base.draw_widget(wb, cr, img, 0, 0, width, height)
         wibox.layout.base.draw_widget(wb, cr, graph, 0, 0, width, height)
     end
-    vicious.register(graph, vicious.widgets.cpu, '$1', 2)
-    local L = wibox.layout.margin()
-    L:set_widget(base)
-    L.fit = function() return 0,61 end
-    return L
+    vicious.register(graph, vicious.widgets.cpu, '$1', 5)
+    local layout = wibox.layout.margin()
+    layout:set_widget(base)
+    layout.fit = function() return 0,61 end
+    return layout
 end
 
 --- Create widgets
@@ -70,11 +125,7 @@ local function new_widget(args)
    return w,t
 end
 
---- Return widgets layout
-local function new()
-   vicious.cache(vicious.widgets.cpu)
-   vicious.cache(vicious.widgets.uptime)
-   
+local function draw()
    local layout={}
    layout["usage"] = wibox.layout.align.horizontal()
    layout["load"] = wibox.layout.align.horizontal()
@@ -83,18 +134,18 @@ local function new()
    local text={}
    local usage={}
     
-   text["load"] = new_widget({text="Load:",align="left",width=58})
-   text["cpu"]  = new_widget({text="Usage:",align="left",width=58})
-   text["temp"] = new_widget({text="Temp:",align="left",width=58})
+   text["load"] = new_widget({ text="Load:", align="left", width=58 })
+   text["cpu"]  = new_widget({ text="Usage:", align="left", width=58 })
+   text["temp"] = new_widget({ text="Temp:", align="left", width=58 })
    
-   usage["cpu"],u  = new_widget({align="right",width=57})
-   usage["load"],l = new_widget({align="right",width=57})
-   usage["temp"],t = new_widget({align="right",width=57})
+   usage["cpu"],c  = new_widget({ align="right", width=57 })
+   usage["load"],l = new_widget({ align="right", width=57 })
+   usage["temp"],t = new_widget({ align="right", width=57 })
     
    vicious.register(t, vicious.contrib.sensors, '$1℃', 30, 'Core0 Temp')
-   vicious.register(l, vicious.widgets.uptime, '$4', 30)
-   vicious.register(u, vicious.widgets.cpu, '$1%', 4)
-    
+   vicious.register(l, vicious.widgets.uptime, '$4', 60)
+   vicious.register(c, vicious.widgets.cpu, '$1%', 5)
+   
    layout["usage"]:set_left(text["cpu"])
    layout["usage"]:set_right(usage["cpu"])
 
@@ -112,5 +163,15 @@ local function new()
 
    return layout["main"]
 end
+
+--- Return widgets layout
+local function new(menu)
+	vicious.cache(vicious.widgets.cpu)
+	vicious.cache(vicious.widgets.uptime)
+
+	local widget = draw()
+	return widget
+end
+
 
 return setmetatable(module, { __call = function(_, ...) return new(...) end })
