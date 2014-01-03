6fcd3e3dcbd70738d935db29e8d5b6ad43a7f6d6
minde <mindeunix@gmail.com>
2014-01-03 16:59:11 +0200

    Make it clean.

--- widgets/sys/cpu.lua
+++ widgets/sys/cpu.lua
@@ -1,81 +1,32 @@
 --[[
         File:      widgets/sys/cpu.lua
-        Date:      2013-12-18
+        Date:      2014-01-03
       Author:      Mindaugas <mindeunix@gmail.com> http://minde.gnubox.com
    Copyright:      Copyright (C) 2013 Free Software Foundation, Inc.
      Licence:      GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
         NOTE:      -------
-            TODO:
-            1: echo 'return collectgarbage("count")' | awesome-client
 --]]
 
 local awful      = require("awful")
 local vicious    = require("extern.vicious")
 local beautiful  = require("beautiful")
 local wibox      = require("wibox")
-local line_graph = require("extern.graph.line_graph")
+local graph      = require("extern.graph.line_graph")
+local common     = require("widgets.sys.common")
 
 local module = {}
 
--- Get CPU usage
-local cpu = {}
-local cpu_usage  = {}
-local cpu_total  = {}
-local cpu_active = {}
-function module.cpuUsage()
-    local cpu_lines = {}
-    -- Get CPU stats
-    local f = io.open("/proc/stat")
-    for line in f:lines() do
-        if string.sub(line, 1, 3) ~= "cpu" then break end
-        cpu_lines[#cpu_lines+1] = {}
-        for i in string.gmatch(line, "[%s]+([^%s]+)") do
-            table.insert(cpu_lines[#cpu_lines], i)
-        end
-    end
-    f:close()
+-- Update widgets interval in seconds
+module.update = {
+    graph = 5,
+    load = 60,
+    temp = 30,
+    cpu = 6
+}
 
-    -- Ensure tables are initialized correctly
-    for i = #cpu_total + 1, #cpu_lines do
-        cpu_total[i]  = 0
-        cpu_usage[i]  = 0
-        cpu_active[i] = 0
-    end
-
-    for i, v in ipairs(cpu_lines) do
-        -- Calculate totals
-        local total_new = 0
-        for j = 1, #v do
-            total_new = total_new + v[j]
-        end
-        local active_new = total_new - (v[4] + v[5])
-
-        -- Calculate percentage
-        local diff_total  = total_new - cpu_total[i]
-        local diff_active = active_new - cpu_active[i]
-
-        if diff_total == 0 then diff_total = 1E-6 end
-        cpu_usage[i]      = math.floor((diff_active / diff_total) * 100)
-
-        -- Store totals
-        cpu_total[i]   = total_new
-        cpu_active[i]  = active_new
-    end
-    dbg.dump(cpu_usage)
-    return cpu_usage
-end
-
--- Timer
-function module.timer()
-    local wtimer = timer{ timeout = 5 }
-    wtimer:connect_signal("timeout", function() end)
-    wtimer:start()
-end
-
---
--- Graph
+-- Graph widget
 local function widget_graph()
-    local graph = line_graph()
+    local graph = graph()
     graph:set_height(61)
     graph:set_width(120)
     graph:set_graph_line_color("#0036B8")
@@ -89,89 +40,55 @@ local function widget_graph()
         wibox.layout.base.draw_widget(wb, cr, img, 0, 0, width, height)
         wibox.layout.base.draw_widget(wb, cr, graph, 0, 0, width, height)
     end
-    vicious.register(graph, vicious.widgets.cpu, '$1', 5)
+    vicious.register(graph, vicious.widgets.cpu, '$1', module.update.graph)
     local layout = wibox.layout.margin()
     layout:set_widget(base)
     layout.fit = function() return 0,61 end
     return layout
 end
-
---- Create widgets
--- @param: text
-local function new_widget(args)
-   local args = args or {}
-   local text = args.text or "N/A"
-   local width = args.width or 40
-   local height = args.height or 12
-   local valign = args.valign or beautiful.widget_text_valign or "center"
-   local align = args.align or beautiful.widget_text_align or "center"
-
-   local t = wibox.widget.textbox()
-   local w = wibox.widget.background()
-   local b = wibox.widget.background()
-   local m = wibox.layout.margin()
-   
-   t:set_align(align)
-   t:set_valign(valign)
-   t.fit = function() return width,height end
-   t:set_text(text)
-   b:set_bg("#001520")
-   b:set_widget(t)
-   m:set_margins(1)
-   m:set_widget(b)
-   w:set_widget(m)
-   w:set_bg("#000000")
-   
-   return w,t
+-- CPU Load
+local function widget_cpuLoad()
+    widget,text  = common.new_widget({ align="right", width=57 })
+    vicious.register(text, vicious.widgets.uptime, '$4',  module.update.load)
+    return widget
 end
-
-local function draw()
-   local layout={}
-   layout["usage"] = wibox.layout.align.horizontal()
-   layout["load"] = wibox.layout.align.horizontal()
-   layout["temp"] = wibox.layout.align.horizontal()
-
-   local text={}
-   local usage={}
-    
-   text["load"] = new_widget({ text="Load:", align="left", width=58 })
-   text["cpu"]  = new_widget({ text="Usage:", align="left", width=58 })
-   text["temp"] = new_widget({ text="Temp:", align="left", width=58 })
-   
-   usage["cpu"],c  = new_widget({ align="right", width=57 })
-   usage["load"],l = new_widget({ align="right", width=57 })
-   usage["temp"],t = new_widget({ align="right", width=57 })
-    
-   vicious.register(t, vicious.contrib.sensors, '$1℃', 30, 'Core0 Temp')
-   vicious.register(l, vicious.widgets.uptime, '$4', 60)
-   vicious.register(c, vicious.widgets.cpu, '$1%', 5)
-   
-   layout["usage"]:set_left(text["cpu"])
-   layout["usage"]:set_right(usage["cpu"])
-
-   layout["load"]:set_left(text["load"])
-   layout["load"]:set_right(usage["load"])
-
-   layout["temp"]:set_left(text["temp"])
-   layout["temp"]:set_right(usage["temp"])
-
-   layout["main"] = wibox.layout.fixed.vertical()
-   layout["main"]:add(widget_graph())
-   layout["main"]:add(layout["load"])
-   layout["main"]:add(layout["usage"])
-   layout["main"]:add(layout["temp"])
-
-   return layout["main"]
+-- CPU temp
+local function widget_cpuTemp()
+    widget,text = common.new_widget({ align="right", width=57 })
+    vicious.register(text, vicious.contrib.sensors, '$1℃', module.update.temp, 'Core0 Temp')
+    return widget
+end
+-- CPU usage
+local function widget_cpuUsage()
+    widget,text  = common.new_widget({ align="right", width=57 })
+    vicious.register(text, vicious.widgets.cpu, '$1%', module.update.cpu)
+    return widget
 end
 
 --- Return widgets layout
 local function new(menu)
-	vicious.cache(vicious.widgets.cpu)
-	vicious.cache(vicious.widgets.uptime)
+    vicious.cache(vicious.widgets.cpu)
+    vicious.cache(vicious.widgets.uptime)
 
-	local widget = draw()
-	return widget
+    local layout_cpu = wibox.layout.align.horizontal()
+    layout_cpu:set_left(common.new_widget({ text="Usage:", align="left", width=58 }))
+    layout_cpu:set_right(widget_cpuUsage())
+
+    local layout_load = wibox.layout.align.horizontal()
+    layout_load:set_left(common.new_widget({ text="Load:",  align="left", width=58 }))
+    layout_load:set_right(widget_cpuLoad())
+
+    local layout_temp = wibox.layout.align.horizontal()
+    layout_temp:set_left(common.new_widget({ text="Temp:",  align="left", width=58 }))
+    layout_temp:set_right(widget_cpuTemp())
+
+    local layout = wibox.layout.fixed.vertical()
+    layout:add(widget_graph())
+    layout:add(layout_load)
+    layout:add(layout_cpu)
+    layout:add(layout_temp)
+
+    return layout
 end
-
 
 return setmetatable(module, { __call = function(_, ...) return new(...) end })
