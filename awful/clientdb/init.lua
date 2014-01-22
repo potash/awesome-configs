cab1e420766fb7f3886fa9387094fed463df9eee
minde <mindeunix@gmail.com>
2014-01-22 23:24:47 +0200

    Try to fix issue #1

--- awful/clientdb/init.lua
+++ awful/clientdb/init.lua
@@ -1,6 +1,6 @@
 --[[
         File:      extern/awfuldb/init.lua
-        Date:      2014-01-02
+        Date:      2014-01-22
       Author:      Mindaugas <mindeunix@gmail.com> http://minde.gnubox.com
    Copyright:      Copyright (C) 2014 Free Software Foundation, Inc.
      Licence:      GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
@@ -8,34 +8,31 @@
 --]]
 
 local setmetatable = setmetatable
-local table = table
-local client = client
-
-local awful   = require("awful")
-local naughty = require("naughty")
-
--- The path used by require to search for a C loader.
-package.cpath = awful.util.getdir("config").."/awful/clientdb/?.so;" .. package.cpath
-local has_sqlite3, sqlite3 = pcall(require, "lsqlite3")
+local table        = table
+local client       = client
+local awful        = require("awful")
+local naughty      = require("naughty")
+local has_DBI, DBI = pcall(require, "DBI")
 
 local module = {}
 module.db = awful.util.getdir("config").."/awful/clientdb/rules.db"
 
---- Displays database notifications.
--- @param msg Text to display
-function module.notify(msg)
+-- Displays database notifications.
+local function notify(msg)
     naughty.notify({ title = "Database", text = msg, border_color = "#FFB111", timeout = 5 })
 end
 
 -- Check if required module loaded
-if not has_sqlite3 then
-    module.notify('There is an error loading LuaSQLite3 module')
+if not has_DBI then
+    module.notify('There is an error loading Lua DBI module')
 end
 
 -- Prepare database
 local function prepare()
-    local db = sqlite3.open(module.db)
-    db:exec[[
+    local dbh = DBI.Connect('SQLite3', module.db)
+    -- set the autocommit flag (this is turned off by default)
+    dbh:autocommit(true)
+    local sth = dbh:prepare([[
         CREATE TABLE IF NOT EXISTS rules(
            id                   INTEGER       PRIMARY KEY AUTOINCREMENT UNIQUE,
            tag                  INTEGER( 2 )  DEFAULT ( 1 ),
@@ -61,82 +58,53 @@ local function prepare()
            height               INTEGER( 5 )  DEFAULT ( 0 ),
            x                    INTEGER( 5 )  DEFAULT ( 0 ),
            y                    INTEGER( 5 )  DEFAULT ( 0 ) 
-    )]]
+    )]])
+    sth:execute()
     -- CREATE UNIQUE INDEX is its own statement and cannot be used within a CREATE TABLE statement.
-    db:exec[[CREATE UNIQUE INDEX IF NOT EXISTS id ON rules(name, class, instance, role)]]
-
-    return db
+    local sth = dbh:prepare([[CREATE UNIQUE INDEX IF NOT EXISTS id ON rules(name, class, instance, role)]])
+    sth:execute()
+    sth:close()
+    return dbh
 end
 
 --- Save the client.
 -- @param c Client
 function module.save(c)
-    if not has_sqlite3 then return end
-    local db = sqlite3.open(module.db)
-    local tag = awful.tag.getidx(awful.tag.selected(1))
+    if not has_DBI then return end
+    local dbh = DBI.Connect('SQLite3', module.db)
+    local c = c or client.focused
+    local tag,geo = awful.tag.getidx(c:tags()[1]), c.geometry(c)
+    -- Optimize values
     local function optimize(obj)
         if type(obj) == "string" then return obj
         elseif type(obj) == "boolean" and obj then return 1
         elseif type(obj) == "boolean" and not obj then return 0
         else return "" end
     end
-
     -- The REPLACE command is an alias for the "INSERT OR REPLACE" variant of the INSERT command
     -- This only works if you have an unique key on the table
-    local stmt = db:prepare[[ REPLACE INTO rules VALUES (NULL, :tag, :name, :class, 
-                :instance, :role, :type, :skip_taskbar, :minimized, :size_hints_honor, :ontop, 
-                :above, :below, :fullscreen, :maximized_horizontal, :maximized_vertical, :sticky, 
-                :focusable, :float, :screen, :width, :height, :x, :y) ]]
-
-    -- Shit happens ;-)
-    if db:errcode() ~= 0 then 
-        module.notify(db:error_message())
-        db:close()
-        return
-    end
-
-    -- Table with client coordinates. 
-    local geo = c.geometry(c)
-
-    -- Binds the values to statement parameters
-    stmt:bind_names{
-        tag = tag,
-        name = optimize(c.name),
-        class = optimize(c.class),
-        instance = optimize(c.instance),
-        role = optimize(c.role),
-        type = optimize(c.type),
-        skip_taskbar = optimize(c.skip_taskbar),
-        minimized = optimize(c.minimized),
-        size_hints_honor = optimize(c.size_hints_honor),
-        ontop = optimize(c.ontop),
-        above = optimize(c.above),
-        below = optimize(c.below),
-        fullscreen = optimize(c.fullscreen),
-        maximized_horizontal = optimize(c.maximized_horizontal),
-        maximized_vertical = optimize(c.maximized_vertical),
-        sticky = optimize(c.sticky),
-        focusable = optimize(c.focusable),
-        float = optimize(awful.client.floating.get(c)),
-        screen = c.screen,
-        width = geo.width,
-        height = geo.height,
-        x = geo.x,
-        y = geo.y
-    }
-    stmt:step()
-    stmt:finalize()
-    module.notify("Client saved.")
-    db:close() -- Close database
+    local sth = dbh:prepare('REPLACE INTO rules VALUES (NULL, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)')
+    -- execute the handle with bind parameters
+    sth:execute(
+        tag, optimize(c.name), optimize(c.class), optimize(c.instance), optimize(c.role),
+        optimize(c.type), optimize(c.skip_taskbar), optimize(c.minimized),
+        optimize(c.size_hints_honor), optimize(c.ontop), optimize(c.above),
+        optimize(c.below), optimize(c.fullscreen), optimize(c.maximized_horizontal),
+        optimize(c.maximized_vertical), optimize(c.sticky), optimize(c.focusable),
+        optimize(awful.client.floating.get(c)), c.screen, geo.width, geo.height, geo.x, geo.y
+    )
+    -- Commit the transaction
+    dbh:commit()
+    -- Finish and close the handles.
+    sth:close()
+    dbh:close()
+    notify("Client saved.")
 end
 
 --- Load windows rules from the database.
--- @param rules table (awful.rules.rules)
--- @param tags tags table (awful.tag.gettags())
-function module.load(rules, tags)
-    if not has_sqlite3 then return end
-    local db = prepare()
-
+function module.load()
+    if not has_DBI then return end
+    local dbh = prepare()
     -- Optimize values
     local function optimize(obj)
         if obj == "" then return nil
@@ -144,41 +112,34 @@ function module.load(rules, tags)
         elseif obj == 0 then return false
         else return obj end
     end
-
-    -- Get all items
-    for item in db:nrows("SELECT * FROM rules") do
+    -- Create a select handle
+    local sth = dbh:prepare('SELECT * FROM rules')
+    sth:execute()
+    for item in sth:rows(true) do
         -- Create new table
         local newrule = {
             rule = {
-                name = optimize(item.name), class = optimize(item.class),
-                instance = optimize(item.instance), role = optimize(item.role),
+                name = optimize(item.name), class = optimize(item.class), instance = optimize(item.instance), role = optimize(item.role),
             },
             properties = {
-                floating = optimize(item.float),
-                skip_taskbar = optimize(item.skip_taskbar),
-                size_hints_honor = optimize(item.size_hints_honor),
-                ontop = optimize(item.ontop),
-                above = optimize(item.above),
-                below = optimize(item.below),
-                fullscreen = optimize(item.fullscreen),
-                maximized_horizontal = optimize(item.maximized_horizontal),
-                maximized_vertical = optimize(item.maximized_vertical),
-                sticky = optimize(item.sticky),
-                focusable = optimize(item.focusable),
-                tag = tags[item.tag],
+                floating = optimize(item.float), skip_taskbar = optimize(item.skip_taskbar),
+                size_hints_honor = optimize(item.size_hints_honor), ontop = optimize(item.ontop),
+                above = optimize(item.above), below = optimize(item.below), fullscreen = optimize(item.fullscreen), 
+                maximized_horizontal = optimize(item.maximized_horizontal), maximized_vertical = optimize(item.maximized_vertical),
+                sticky = optimize(item.sticky),focusable = optimize(item.focusable), tag = awful.tag.gettags(item.screen)[item.tag]
             }
         }
         -- Set coordinates (if x/y isn't 0 and client is floating state)
-        if item.float == 1 and item.x ~= 0 and item.y ~= 0 then
-            newrule.callback = function(_c)
+        if optimize(item.float) and item.x ~= 0 and item.y ~= 0 then
+                newrule.callback = function(_c)
                 _c:geometry({ width = item.width , height = item.height, x = item.x, y = item.y })
             end
         end
-        table.insert(rules, newrule)
+        table.insert(awful.rules.rules, newrule)
     end
-
-    -- Close database.
-    if db then db:close() end
+    -- finish and close the handles.
+    sth:close()
+    dbh:close()
 end
 
 local function new()
